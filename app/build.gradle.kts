6906fbc91ea1f181c47ba79677464beb7938fc6f
arfoux <farosazy@gmail.com>
2026-09-02 22:18:26 +0700

    chore: bump v0.5.13 -> v0.5.14 (keystore fix need new build)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.d4viddf.hyperbridge"
         minSdk = 35
         targetSdk = 37
-        versionCode = 39
-        versionName = "0.5.13"
+        versionCode = 40
+        versionName = "0.5.14"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
